c981fc3e98715e122265907dbf75334655bb6884
Abdelrahman Elsnosy <snosyw2@gmail.com>
2021-12-23 22:03:25 +0200

    editing Components Class

--- src/main/java/topology/Components.java
+++ src/main/java/topology/Components.java
@@ -8,7 +8,6 @@ public class Components {
 	protected Map<String, Double>numData;
 	protected Map<String, String>netList;
 	public Components(String id,String type) {
-		// TODO Auto-generated constructor stub
 		this.id=id;
 		this.type=type;
 	}
@@ -23,17 +22,5 @@ public class Components {
 	}
 	public String getType() {
 		return this.type;
-	}
-	public Map<String, Double> getNumData() {
-		return numData;
-	}
-	public void setNumData(Map<String, Double> numData) {
-		this.numData = numData;
-	}
-	public Map<String, String> getNetList() {
-		return netList;
-	}
-	public void setNetList(Map<String, String> netList) {
-		this.netList = netList;
 	}
 }
